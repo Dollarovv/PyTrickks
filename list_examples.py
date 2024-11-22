3417562814a0cb024d8a8d44dedb864394ece071
Dollarovv <105046455+Dollarovv@users.noreply.github.com>
2024-11-23 00:00:00 +0500

    'add file list_examples.py'

---
+++ list_examples.py
@@ -0,0 +1,4 @@
+from tricks.list_tricks import list_comprehension_example, flatten_list
+
+print("List Comprehension Example:", list_comprehension_example())
+print("Flatten List Example:", flatten_list([[1, 2], [3, 4], [5]]))
